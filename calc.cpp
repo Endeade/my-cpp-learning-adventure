275bb7595cd8b0ff95c1f06512cb70823d1e713d
Endeade <120813259+Endeade@users.noreply.github.com>
2023-05-28 17:41:08 +0200

    calc update

--- calc.cpp
+++ calc.cpp
@@ -13,12 +13,24 @@ void main() {
     float results;
     float resultd;
     float resultm;
+    float resultsqrtone;
+    float resultsqrttwo;
+    float resultnonesquare;
+    float resultntwosquare;
     resulta = numone + numtwo;
     results = numone - numtwo;
     resultd = numone / numtwo;
     resultm = numone * numtwo;
+    resultsqrtone = sqrt(numone);
+    resultsqrtone = sqrt(numtwo);
+    resultnonesquare = numone * numone;
+    resultntwosquare = numtwo * numtwo;
     std::cout << numone << "+" << numtwo << "=" << resulta << std::endl;
     std::cout << numone << "-" << numtwo << "=" << results << std::endl;
     std::cout << numone << "*" << numtwo << "=" << resultm << std::endl;
     std::cout << numone << "/" << numtwo << "=" << resultd << std::endl;
+    std::cout << numone << " squared is " << resultnonesquare << std::endl;
+    std::cout << numtwo << " squared is " << resultntwosquare << std::endl;
+    std::cout << "Square root of " << numone << " is " << resultsqrtone << std::endl;
+    std::cout << "Square root of " << numtwo << " is " << resultsqrttwo << std::endl;
 }
